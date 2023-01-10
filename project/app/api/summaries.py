60ec80a629515b8f5804f3597c48aaf9f9e23eec
Derek Anderson <backupacct4664@gmail.com>
2023-01-10 13:05:06 -0600

    Update get/post/put tests to handle invalid url's and error for an id of 0

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema, SummaryUpdatePayloadSchema
@@ -19,7 +19,7 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
 
 
 @router.get("/{id}/", response_model=SummarySchema)
-async def read_summary(id: int) -> SummarySchema:
+async def read_summary(id: int = Path(..., gt=0)) -> SummarySchema:
     summary = await crud.get(id)
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
@@ -44,7 +44,7 @@ async def delete_summary(id: int) -> SummaryResponseSchema:
 
 
 @router.put("/{id}/", response_model=SummarySchema)
-async def update_summary(id: int, payload: SummaryUpdatePayloadSchema) -> SummarySchema:
+async def update_summary(payload: SummaryUpdatePayloadSchema, id: int = Path(..., gt=0)) -> SummarySchema:
   summary = await crud.put(id, payload)
   if not summary:
     raise HTTPException(status_code=404, detail="Summary not found")
