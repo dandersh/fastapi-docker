a1b620f8d458cf8871110f10f3f75b69905d6107
Derek Anderson <backupacct4664@gmail.com>
2023-01-23 16:06:35 -0600

    Fix sorting/formatting that broke gh action.

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -3,10 +3,14 @@ from typing import List
 from fastapi import APIRouter, BackgroundTasks, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import (SummaryPayloadSchema, SummaryResponseSchema,
-                                 SummaryUpdatePayloadSchema)
 from app.models.tortoise import SummarySchema
 from app.summarizer import generate_summary
+
+from app.models.pydantic import (  # isort:skip
+    SummaryPayloadSchema,
+    SummaryResponseSchema,
+    SummaryUpdatePayloadSchema,
+)
 
 router = APIRouter()
 
